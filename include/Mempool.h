a62aa5b232fb526fccd48b2713e46c478d0061c1
shendrew <andrewsh190@gmail.com>
2026-05-12 23:40:00 -0400

    mempool namespace

--- include/Mempool.h
+++ include/Mempool.h
@@ -2,37 +2,39 @@
 
 #include <stddef.h>
 
-// Mempool header
-class ARTMempool {
+namespace ART::alloc {
+    // Mempool header
+    class Mempool {
 
-public:
-    void *allocate(size_t size) {
-        return nullptr;
-    }
+    public:
+        void *allocate(size_t size) {
+            return nullptr;
+        }
 
-    void deallocate(void *ptr, size_t size) {
-        // deallocate memory
-    }
-};
-
-
-inline ARTMempool globalPool;
-
-// Mempool Allocator Proxy (very low mem usage)
-template <typename T>
-class MempoolProxy {
-
-public:
-    template <typename U>
-    struct rebind {
-        using other = MempoolProxy<U>;
+        void deallocate(void *ptr, size_t size) {
+            // deallocate memory
+        }
     };
 
-    T* allocate(std::size_t n) {
-        return static_cast<T*>(globalPool.allocate(sizeof(T) * n));
-    }
 
-    void deallocate(T* ptr, std::size_t n) {
-        globalPool.deallocate(ptr, sizeof(T) * n);
-    }
-};
+    inline Mempool globalPool;
+
+    // Mempool Allocator Proxy (very low mem usage)
+    template <typename T>
+    class MempoolProxy {
+
+    public:
+        template <typename U>
+        struct rebind {
+            using other = MempoolProxy<U>;
+        };
+
+        T* allocate(std::size_t n) {
+            return static_cast<T*>(globalPool.allocate(sizeof(T) * n));
+        }
+
+        void deallocate(T* ptr, std::size_t n) {
+            globalPool.deallocate(ptr, sizeof(T) * n);
+        }
+    };
+}
